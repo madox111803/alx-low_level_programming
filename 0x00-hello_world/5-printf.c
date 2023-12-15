7458cb120654308fc5a4eea8647fb1c4063257cc
madox111803 <madox111803@gmail.com>
2023-12-15 14:48:35 -0800

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #! /bin/bash
 #include <stdio.h>
 
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
 int main(void) {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
