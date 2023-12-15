227b80e73086c1e5d8eb3c9ee6eee71f2f0aa452
madox111803 <madox111803@gmail.com>
2023-12-15 14:46:39 -0800

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-#!/bin/bash
-include <stdio.h>
+#! /bin/bash
+#include <stdio.h>
 
-int main() {
+int main(void) {
 
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
 }
