59605b996399b92c04e8e9721bb478237c250039
madox111803 <madox111803@gmail.com>
2023-12-18 14:29:56 -0800

    C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase\n
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char harf;
+
+	for (harf = 'a'; harf <= 'z'; harf++)
+
+		putchar (harf);
+
+	putchar('\n')
+return (0);
+}
