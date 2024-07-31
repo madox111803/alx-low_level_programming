955e18ab6e447e4f246055d33a21faf4bab51096
madox111803 <madox111803@gmail.com>
2024-07-31 20:06:33 +0000

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+/**
+*main - Prints all single digits with , and space followed by new line
+*
+*Return: returns 0
+*/
 
 int main() {
     int i;
     
-    for (i = 0; i < 10; i++){
-        putchar(i + '0');
-        if (i<9) {
+    for (i = '0'; i < '10'; i++){
+        putchar(i);
+        if (i < 9) {
             putchar(',');
             putchar(' ');
         }
