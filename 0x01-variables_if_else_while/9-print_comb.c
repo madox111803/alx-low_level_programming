a6e96a518b7c1a5967028840f59978625af30dde
madox111803 <madox111803@gmail.com>
2024-07-31 20:08:31 +0000

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,9 +8,9 @@
 int main() {
     int i;
     
-    for (i = '0'; i < '10'; i++){
+    for (i='0'; i<'10'; i++){
         putchar(i);
-        if (i < 9) {
+        if (i<9) {
             putchar(',');
             putchar(' ');
         }
