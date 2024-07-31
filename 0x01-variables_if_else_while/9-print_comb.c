3496785e9ccd177f07d1ef2210224054053ad78b
madox111803 <madox111803@gmail.com>
2024-07-31 20:11:16 +0000

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,17 +4,16 @@
 *
 *Return: returns 0
 */
-
 int main() {
-    int i;
+	int i;
     
-    for (i='0'; i<'10'; i++){
-        putchar(i);
-        if (i<9) {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+	for (i = 0; i < 10; i++){
+		putchar(i + '0');
+		if (i<9) {
+			putchar(',');
+			putchar(' ');
+		}
+	}
     
-    return 0;
+	return 0;
 }
